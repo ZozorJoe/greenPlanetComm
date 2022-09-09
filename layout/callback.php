cf293524c67882fc38c68553c037e1f145e98ba4
Charla19 <charlap20.aps2a@gmail.com>
2022-09-09 11:37:25 +0300

    form control

--- layout/callback.php
+++ layout/callback.php
@@ -1,4 +1,37 @@
 <!-- Callback form -->
+<?php 
+  //Alerts variables
+  $msg = "";
+  $msgClass = "";
+  //checking for the submit 
+  if(filter_has_var(INPUT_POST, 'submit-msg')){
+    //get the form data
+    $name = htmlspecialchars($_POST["nomComplete"]);
+    $adresse_email = htmlspecialchars($_POST["adresse_email"]);
+    $message_subject = htmlspecialchars($_POST["message_subject"]);
+    $message_content = htmlspecialchars($_POST["message_content"]);
+
+    // check required fields
+    if(!empty($name) && !empty($adresse_email) && !empty($message_subject) && !empty($message_content)){
+      //check email
+      if(filter_var($adresse_email, FILTER_VALIDATE_EMAIL)){
+        //passed
+        $msg = 'Votre mail a bien été envoyé!';
+        $msgClass = 'alert-success';
+        
+      } else {
+        //failed 
+        $msg = 'Please, enter a valid email adress!';
+        $msgClass = 'alert-danger';
+      }
+    } else {
+      //failed
+      $msg = 'Please, fill all the fields!';
+      $msgClass = 'alert-danger';
+    }
+  }
+
+?>
 <div class="callback-form">
 
 <div class="container">
@@ -20,16 +53,19 @@
     <div class="col-md-12">
 
       <div class="contact-form">
+      <?php if($msg != ""): ?>
+        <div class="alert <?php echo $msgClass ; ?>"> <?php echo $msg ; ?></div>
+      <?php endif; ?>
 
-        <div id="contact" >
+        <div id="contact">
         <div id="message_notification"></div>
           <div class="row">
 
             <div class="col-lg-4 col-md-12 col-sm-12">
-            <form action="" method="post">
+            <form action="#contact" method="post">
               <fieldset>
 
-                <input name="nomComplete" type="text" class="form-control" id="nom-contact" placeholder=<?php echo $lang['callback-nom']; ?>" required>
+                <input name="nomComplete" type="text" class="form-control" id="nom-contact" placeholder="<?php echo $lang['callback-nom']; ?>" value="<?php echo isset($_POST['nomComplete']) ? $name : ''; ?>">
 
               </fieldset>
               <div class="nameNotif"></div>
@@ -40,7 +76,7 @@
 
               <fieldset>
 
-                <input name="adresse_email" type="text"  <?php $invalid_class_mail ?? "" ?> class="form-control" id="email-contact" pattern="[^ @]*@[^ @]*" placeholder="<?php echo $lang['callback-mail']; ?>" required>
+                <input name="adresse_email" type="text"  <?php $invalid_class_mail ?? "" ?> class="form-control" id="email-contact" placeholder="<?php echo $lang['callback-mail']; ?>" value="<?php echo isset($_POST['adresse_email']) ? $adresse_email : ''; ?>">
 
               </fieldset>
               <div class="emailNotif"></div>
@@ -51,7 +87,7 @@
 
               <fieldset>
 
-                <input name="message_subject" type="text" class="form-control" id="subject-contact" placeholder="<?php echo $lang['callback-subject']; ?>" required>
+                <input name="message_subject" type="text" class="form-control" id="subject-contact" placeholder="<?php echo $lang['callback-subject']; ?>" value="<?php echo isset($_POST['message_subject']) ? $message_subject : ''; ?>">
 
               </fieldset>
               <div class="subjectNotif"></div>
@@ -62,7 +98,7 @@
 
               <fieldset>
 
-                <textarea name="message_content" rows="6" class="form-control" id="message-Contact" placeholder="<?php echo $lang['callback-msg']; ?>" required></textarea>
+                <textarea name="message_content" rows="6" class="form-control" id="message-Contact" placeholder="<?php echo $lang['callback-msg']; ?>"><?php echo isset($_POST['message_content']) ? $message_content : ''; ?></textarea>
 
               </fieldset>
               <div class="messageNotif"></div>
@@ -73,7 +109,7 @@
 
               <fieldset>
 
-                <button  id="submit" class="border-button form_submit_message"><?php echo $lang['callback-btn']; ?></button>
+                <button  id="submit" name="submit-msg" class="border-button form_submit_message"><?php echo $lang['callback-btn']; ?></button>
                 
               </fieldset>
               <div class="submitNotif"></div>
@@ -96,111 +132,3 @@
 
 </div>
 
-<script>
-  $(document).ready(function(){
-    var nomInput =document.querySelector("#nom-contact")
-    var emailInput =document.querySelector("#email-contact")
-    var subjectInput =document.querySelector("#subject-contact")
-    var messageInput =document.querySelector("#message-Contact")
-        nomInput.addEventListener('input',function () {
-        document.getElementById("nomNotif").style.display = "none";
-        })
-        emailInput.addEventListener('input',function () {
-        document.getElementById("emailNotif").style.display = "none";
-        })
-        subjectInput.addEventListener('input',function () {
-        document.getElementById("subjectNotif").style.display = "none";
-        })
-        messageInput.addEventListener('input',function () {
-        document.getElementById("messageNotif").style.display = "none";
-        })
-    $(document).on('click','.form_submit_message', function(){
-      console.log('test')
-      var nom = $('input[name="nomComplete"]').val();
-      var email = $('input[name="adresse_email"]').val();
-      var subject = $('input[name="message_subject"]').val();
-      var message = $('input[name="message_content"]').val();
-      var langues = $('input[name="langue"]').val();
-
-   
-      var data ={
-        nom:nom,
-        email:email,
-        subject:subject,
-        message:message,
-        langue:langues
-      }
-      
-      if(nom==""){
-        $('.nameNotif').html(`
-        <div class="invalid-feedback">Veuillez entrer votre nom!</div>
-        `);
-          }  else if(email==""){
-            $('.mailNotif').html(`
-            <div class="invalid-feedback">Veuillez entrer votre adresse mail!</div>
-            `);
-          }  else if(subject==""){
-            $('.subjectNotif').html(`
-            <div class="invalid-feedback">N'oubliez pas de mentionner l'objet du message!</div>
-            `);
-          }  else if(message==""){
-            $('.messageNotif').html(`
-            <div class="invalid-feedback">Veuillez rediger ici votre message!</div>
-            `);
-          } else if(nom!="" && email!="" && subject!="" && message!=""){
-            $.ajax({
-            type: 'POST', // http method
-            data: data,
-            url:'controlleur/callback.php',
-            success: function(response) {
-              console.log(response)
-              let result =JSON.parse(response)
-              $('#notification').html(`
-              <hr>
-              <span class="alert alert-success text-center" style="font-size: 1em;">${result.msg}</span>
-              <br>
-              `);
-              $('input[name="nom"]').val(null);
-            $('input[name="emailadress"]').val(null);
-            $("#messageContact").val(null);
-            },
-            })
-          }
-      })
-
-
-    $(document).on('click','.form_submit', function(){
-      console.log('test')
-      var nom = $('input[name="nomComplete"]').val();
-      var email = $('input[name="adresse_email"]').val();
-      var subject = $('input[name="subject"]').val();
-      var message = $("#message1").val();
-      var langues = $('input[name="langue"]').val();
-
-      var data ={
-        nom:nom,
-        email:email,
-        message:message,
-        subject:subject,
-        langue:langues
-
-      }
-      $.ajax({
-      type: 'POST', // http method
-      data: data,
-      url:'controlleur/callback.php',
-      success: function(response) {
-        console.log(response)
-        let result =JSON.parse(response)
-        $('#message_notification').html(`
-        <div class="alert alert-success text-center" style="font-size: 1em;">${result.msg}</div>
-        `);
-        $('input[name="nomComplete"]').val(null);
-          $('input[name="adresse_email"]').val(null);
-          $('input[name="subject"]').val(null);
-          $("#message1").val(null);
-      },
-      })
-    })
-  })
-  </script>
